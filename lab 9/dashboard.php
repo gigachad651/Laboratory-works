828df51501693b789804de5e771de5dd577c0612
gigachad651 <artyom.birykov18@gmail.com>
2024-12-08 21:53:32 +0700

    Update dashboard.php

--- lab 9/dashboard.php
+++ lab 9/dashboard.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['user'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Панель управления</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet"> <!-- Подключение шрифта Poppins -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet"> 
     <style>
         * {
             box-sizing: border-box;
